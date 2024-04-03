52cc513613716821f9b74d5cf4a05a5c3f1b9797
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-02 19:19:13 -0500

    initial commit

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahreinhietToCelsius(double fahreinhiet)
+        {
+            return (fahreinhiet - 32) / 1.8;
+        }
+        public static double CelsiusToFahreinhiet(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
