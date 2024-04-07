920f0fa39f1a39fffad378f46e2efe8cb1711916
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-07 18:17:18 -0500

    called methods in main method

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a Celsius value to convert");
+            double.TryParse(Console.ReadLine(), out var C2F);
+            Console.WriteLine( TempConverter.CelsiusToFahreinhiet(C2F));
+            Console.WriteLine("Enter a Fahreinhiet value to convert");
+            double.TryParse(Console.ReadLine(), out var F2C);
+            Console.WriteLine( TempConverter.FahreinhietToCelsius(F2C));
         }
     }
 }
